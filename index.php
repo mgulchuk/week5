cf37717f8cefb5bae1fa007add0d9a9c8ae13c09
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-05-04 16:10:08 -0400

    added default view

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Start a session
+session_start();
+
+// Require the autoload file
+require_once("vendor/autoload.php");
+
+// Instantiate the F3 Base Class
+$f3 = Base::instance();
+
+// Default route
+$f3->route('GET /', function()
+{
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
+
+// Run F3
+$f3->run();
+
